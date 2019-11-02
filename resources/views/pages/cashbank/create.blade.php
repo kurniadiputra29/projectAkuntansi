9576a7c856146564aa46503d352991af13ab467f
adibest <ahmadjayadibya@gmail.com>
2019-11-02 22:40:12 +0700

    penambahannya masih absurd

--- resources/views/pages/cashbank/create.blade.php
+++ resources/views/pages/cashbank/create.blade.php
@@ -31,12 +31,13 @@
         </div>
       </div>
 
-      <div class="row">
+      <div class="row" id="app">
         <div class="col-md-12">
+          <form class="forms-sample" action="{{route('cashbank.store')}}" method="post">
+            @csrf
             <div class="card">
                 <div class="card-header"><h3>Basic form elements</h3></div>
                 <div class="card-body">
-                  <form class="forms-sample">
                     <div class="row input-group-primary">
                       <div class="col-md-4">
                         <div class="form-group">
@@ -70,108 +71,88 @@
                       </div>
                     </div>
 
-                    <div style="margin-bottom: 100px;"></div>
+                    <div
+                      class="row"
+                      v-for="(cashbank, index) in cashbanks"
+                      :key="index"
+                    >
+                      <div class="col-md-3">
+                        <div class="form-group">
+                          <label for="terima_dari">Terima Dari</label>
+                          <input class="form-control" type="text" id="terima_dari" name="terima_dari[]" v-model="cashbank.terima_dari">
+                        </div>
+                      </div>
+                      <div class="col-md-4">
+                        <div class="form-group">
+                          <label for="description">Deskripsi</label>
+                          <input class="form-control" type="text" id="description" name="description[]" v-model="cashbank.description">
+                        </div>
+                      </div>
+                      <div class="col-md-4">
+                        <div class="form-group">
+                          <label for="jumlah">Jumlah</label>
+                          <input class="form-control" type="number" id="jumlah" name="jumlah[]" v-model="cashbank.jumlah">
+                        </div>
+                      </div>
+                      <div class="col-md-1">
+                        <div class="form-group">
+                          <label for="jumlah">Delete</label>
+                          <button class="btn btn-rounded btn-danger" @click="del(index)" type="button"><i class="ik ik-delete"></i></button>
+                        </div>
+                      </div>
+                    </div>
 
-                    <div class="card">
+                    <div class="row">
+                      <div class="col-md-12">
+                        <div class="form-group">
+                          <button class="btn btn-rounded btn-success" @click="add()" type="button"><i class="ik ik-plus-circle"></i></button>
+                        </div>
+                      </div>
+                    </div>
+
+                    {{-- <div class="card">
                       <div class="table-responsive">
                         <table class="table">
                           <thead>
                             <tr>
                               <th>Terima Dari</th>
                               <th>Deskripsi</th>
-                              <th>Pajak</th>
                               <th>Jumlah</th>
                             </tr>
                           </thead>
                           <tbody>
-                            <tr>
-                              <th>
-                                <div class="form-group">
-                                  <input class="form-control" type="text" id="no_transaksi">
-                                </div>
-                              </th>
+                            <tr
+                              v-for="(cashbank, index) in cashbanks"
+                              :key="index"
+                            >
                               <td>
                                 <div class="form-group">
-                                  <input class="form-control" type="text" id="no_transaksi">
+                                  <input class="form-control" type="text" id="terima_dari" name="terima_dari" v-model="cashbank.terima_dari">
                                 </div>
                               </td>
                               <td>
                                 <div class="form-group">
-                                  <select class="form-control" id="setor_ke">
-                                    @foreach ($akun as $key)
-                                      <option>{{$key->nama}}</option>
-                                    @endforeach
-                                  </select>
+                                  <input class="form-control" type="text" id="description" name="description" v-model="cashbank.description">
                                 </div>
                               </td>
                               <td>
                                 <div class="form-group">
-                                  <input class="form-control" type="text" id="no_transaksi">
-                                </div>
-                              </td>
-                            </tr>
-                            <tr>
-                              <th>
-                                <div class="form-group">
-                                  <input class="form-control" type="text" id="no_transaksi">
-                                </div>
-                              </th>
-                              <td>
-                                <div class="form-group">
-                                  <input class="form-control" type="text" id="no_transaksi">
-                                </div>
-                              </td>
-                              <td>
-                                <div class="form-group">
-                                  <select class="form-control" id="setor_ke">
-                                    @foreach ($akun as $key)
-                                      <option>{{$key->nama}}</option>
-                                    @endforeach
-                                  </select>
-                                </div>
-                              </td>
-                              <td>
-                                <div class="form-group">
-                                  <input class="form-control" type="text" id="no_transaksi">
-                                </div>
-                              </td>
-                            </tr>
-                            <tr>
-                              <th>
-                                <div class="form-group">
-                                  <input class="form-control" type="text" id="no_transaksi">
-                                </div>
-                              </th>
-                              <td>
-                                <div class="form-group">
-                                  <input class="form-control" type="text" id="no_transaksi">
-                                </div>
-                              </td>
-                              <td>
-                                <div class="form-group">
-                                  <select class="form-control" id="setor_ke">
-                                    @foreach ($akun as $key)
-                                      <option>{{$key->nama}}</option>
-                                    @endforeach
-                                  </select>
-                                </div>
-                              </td>
-                              <td>
-                                <div class="form-group">
-                                  <input class="form-control" type="text" id="no_transaksi">
+                                  <input class="form-control" type="text" id="jumlah" name="jumlah" v-model="cashbank.jumlah">
                                 </div>
                               </td>
                             </tr>
                           </tbody>
                         </table>
                       </div>
-                    </div>
+                    </div> --}}
+
+                    <div style="margin-bottom: 40px;"></div>
 
                     <div class="row d-flex justify-content-end">
                       <div class="col-md-4">
                         <div class="form-group">
-                          <label>Total</label>
-                          <input class="form-control" type="text" id="no_transaksi">
+                          <label>Total : Rp</label>
+                          <input class="form-control" type="number" name="total" :value="total" readonly>
                         </div>
                       </div>
                     </div>
@@ -181,193 +162,43 @@
                         <a class="btn btn-outline-primary btn-rounded" href="{{ route('cashbank.create') }}">Create</a>
                       </div>
                     </div>
-                  </form>
                 </div>
             </div>
+          </form>
         </div>
-        {{-- <div class="col-md-6">
-            <div class="card">
-                <div class="card-header"><h3>Input Sizes</h3></div>
-                <div class="card-body">
-                    <form>
-                        <div class="form-group row">
-                            <div class="col-sm-12">
-                                <input type="text" class="form-control form-control-lg" placeholder=".form-control-lg">
-                            </div>
-                        </div>
-                        <div class="form-group row">
-                            <div class="col-sm-10">
-                                <input type="text" class="form-control" placeholder=".form-control">
-                            </div>
-                        </div>
-                        <div class="form-group row">
-                            <div class="col-sm-8">
-                                <input type="text" class="form-control form-control-sm" placeholder=".form-control-sm">
-                            </div>
-                        </div>
-                    </form>
-                </div>
-            </div>
-            <div class="card">
-                <div class="card-header"><h3>Text-color</h3></div>
-                <div class="card-body">
-                    <form>
-                        <div class="form-group">
-                            <input type="text" class="form-control form-txt-primary" placeholder=".form-txt-primary">
-                        </div>
-                        <div class="form-group row">
-                            <div class="col-sm-6">
-                                <input type="text" class="form-control form-txt-warning" placeholder=".form-txt-warning">
-                            </div>
-                            <div class="col-sm-6">
-                                <input type="text" class="form-control form-txt-default" placeholder=".form-txt-default">
-                            </div>
-                        </div>
-                        <div class="form-group row">
-                            <div class="col-sm-6">
-                                <input type="text" class="form-control form-txt-danger" placeholder=".form-txt-danger">
-                            </div>
-                            <div class="col-sm-6">
-                                <input type="text" class="form-control form-txt-success" placeholder=".form-txt-success">
-                            </div>
-                        </div>
-                        <div class="row">
-                            <div class="col-sm-6">
-                                <input type="text" class="form-control form-txt-inverse" placeholder=".form-txt-inverse">
-                            </div>
-                            <div class="col-sm-6">
-                                <input type="text" class="form-control form-txt-info" placeholder=".form-txt-info">
-                            </div>
-                        </div>
-                    </form>
-                </div>
-            </div>
-        </div>
-        <div class="col-md-6">
-            <div class="card">
-                <div class="card-header"><h3>Color Inputs</h3></div>
-                <div class="card-body">
-                    <form>
-                        <div class="form-group">
-                            <input type="text" class="form-control form-control-primary" placeholder=".form-control-primary">
-                        </div>
-                        <div class="form-group row">
-                            <div class="col-sm-6">
-                                <input type="text" class="form-control form-control-warning" placeholder=".form-control-warning">
-                            </div>
-                            <div class="col-sm-6">
-                                <input type="text" class="form-control form-control-default" placeholder=".form-control-default">
-                            </div>
-                        </div>
-                        <div class="form-group row">
-                            <div class="col-sm-6">
-                                <input type="text" class="form-control form-control-danger" placeholder=".form-control-danger">
-                            </div>
-                            <div class="col-sm-6">
-                                <input type="text" class="form-control form-control-success" placeholder=".form-control-success">
-                            </div>
-                        </div>
-                        <div class="form-group row">
-                            <div class="col-sm-6">
-                                <input type="text" class="form-control form-control-inverse" placeholder=".form-control-inverse">
-                            </div>
-                            <div class="col-sm-6">
-                                <input type="text" class="form-control form-control-info" placeholder=".form-control-info">
-                            </div>
-                        </div>
-                    </form>
-                </div>
-            </div>
-        </div>
-        <div class="col-md-6">
-            <div class="card">
-                <div class="card-header"><h3>Background-color</h3></div>
-                <div class="card-body">
-                    <form>
-                        <div class="form-group">
-                            <input type="text" class="form-control form-bg-primary" placeholder=".form-bg-primary">
-                        </div>
-                        <div class="form-group row">
-                            <div class="col-sm-6">
-                                <input type="text" class="form-control form-bg-warning" placeholder=".form-bg-warning">
-                            </div>
-                            <div class="col-sm-6">
-                                <input type="text" class="form-control form-bg-default" placeholder=".form-bg-default">
-                            </div>
-                        </div>
-                        <div class="form-group row">
-                            <div class="col-sm-6">
-                                <input type="text" class="form-control form-bg-danger" placeholder=".form-bg-danger">
-                            </div>
-                            <div class="col-sm-6">
-                                <input type="text" class="form-control form-bg-success" placeholder=".form-bg-success">
-                            </div>
-                        </div>
-                        <div class="row">
-                            <div class="col-sm-6">
-                                <input type="text" class="form-control form-bg-inverse" placeholder=".form-bg-inverse">
-                            </div>
-                            <div class="col-sm-6">
-                                <input type="text" class="form-control form-bg-info" placeholder=".form-bg-info">
-                            </div>
-                        </div>
-                    </form>
-                </div>
-            </div>
-        </div>
-
-        <div class="col-md-6">
-            <div class="card" style="min-height: 180px;">
-                <div class="card-header"><h3>Inline forms</h3></div>
-                <div class="card-body">
-                    <form class="form-inline">
-                        <label class="sr-only" for="inlineFormInputName2">Name</label>
-                        <input type="text" class="form-control mb-2 mr-sm-2" id="inlineFormInputName2" placeholder="Shanker Raj">
-
-                        <label class="sr-only" for="inlineFormInputGroupUsername2">Username</label>
-                        <div class="input-group mb-2 mr-sm-2">
-                            <div class="input-group-prepend">
-                                <div class="input-group-text">@</div>
-                            </div>
-                            <input type="text" class="form-control" id="inlineFormInputGroupUsername2" placeholder="Username">
-                        </div>
-                        <div class="form-check mx-sm-2">
-                            <label class="custom-control custom-checkbox">
-                                <input type="checkbox" class="custom-control-input" checked>
-                                <span class="custom-control-label">&nbsp; Remember Me</span>
-                            </label>
-                        </div>
-                        <button type="submit" class="btn btn-primary mb-2">Submit</button>
-                    </form>
-                </div>
-            </div>
-        </div>
-
-        <div class="col-md-6">
-            <div class="card">
-                <div class="card-header"><h3>Validation States</h3></div>
-                <div class="card-body">
-                    <form class="forms-sample">
-                        <div class="row">
-                            <div class="col-sm-6">
-                                <div class="form-group">
-                                    <label for="exampleInputName1">Name</label>
-                                    <input type="text" class="form-control is-valid" id="exampleInputName1" placeholder="Name">
-                                </div>
-                            </div>
-                            <div class="col-sm-6">
-                                <div class="form-group">
-                                    <label for="exampleInputEmail3">Email address</label>
-                                    <input type="email" class="form-control is-invalid" id="exampleInputEmail3" placeholder="Email">
-                                </div>
-                            </div>
-                        </div>
-                    </form>
-                </div>
-            </div>
-        </div> --}}
       </div>
     </div>
   </div>
 
+@endsection
+
+@section('vue')
+	<script type="text/javascript">
+		new Vue({
+			el: '#app',
+			data: {
+				cashbanks: [
+					{terima_dari:"", description:"", jumlah: 0},
+				]
+			},
+			methods: {
+				add() {
+					var cashbanks = {terima_dari:"", description:"", jumlah: 0};
+					this.cashbanks.push(cashbanks);
+				},
+				del(index) {
+					if (index > 0) {
+						this.cashbanks.splice(index, 1);
+					}
+				},
+			},
+			computed: {
+				total() {
+					return this.cashbanks
+					.map( cashbank => cashbank.jumlah )
+					.reduce( (prev, next) => prev + next, 0);
+				},
+			},
+		});
+	</script>
 @endsection
