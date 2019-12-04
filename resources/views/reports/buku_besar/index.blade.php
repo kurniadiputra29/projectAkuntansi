d80c6748e656589a5be40dd621bbf1d56969db24
adibest <ahmadjayadibya@gmail.com>
2019-12-04 20:41:56 +0700

    mengubah tampilan buku besar

--- resources/views/reports/buku_besar/index.blade.php
+++ resources/views/reports/buku_besar/index.blade.php
@@ -49,254 +49,44 @@
               <a type="button" class="btn btn-primary" href="#"><i class="ik ik-printer"></i>Print</a>
             </div>
           </div>
-          <div class="table-container list-table">
-            <div class="report-title">
-              <div class="table-responsive">
-                <table class="account-transactions report-table table" id="account-entry">
-                  <thead class="report-header">
-                    <tr class="bg-secondary font-weight-bold">
-                      <th class="text-light">
-                        Nama Akun / Tanggal
-                      </th>
-                      <th class="text-light">
-                        Transaksi
-                      </th>
-                      <th class="text-light">
-                        Nomor
-                      </th>
-                      <th class="text-light">
-                        Keterangan
-                      </th>
-                      <th class="text-right text-light">
-                        Debit
-                      </th>
-                      <th class="text-right text-light">
-                        Kredit
-                      </th>
-                      <th class="text-right text-light">
-                        Saldo
-                      </th>
-                    </tr>
-                  </thead>
-                  <tbody>
-                    <tr>
-                      <td class="report-subheader-noindent bg-primary text-light" colspan="7">
-                        (1-10001) Cash
-                      </td>
-                    </tr>
-                    <tr>
-                      <td class="bold">
-                        24/11/2019
-                      </td>
-                      <td class="text-left bold">
-                        Saldo Awal
-                      </td>
-                      <td colspan="4"></td>
-                      <td class="text-right bold">
-                        10.500.000,00
-                      </td>
-                    </tr>
-                    <tr class="report-subtotal">
-                      <td class="text-right regular-text" colspan="4">
-                        ((1-10001) Cash) | Saldo Akhir
-                      </td>
-                      <td class="text-right bold">
-                        0,00
-                      </td>
-                      <td class="text-right bold">
-                        0,00
-                      </td>
-                      <td class="text-right bold">
-                        10.500.000,00
-                      </td>
-                    </tr>
-
-                    <tr>
-                      <td class="report-subheader-noindent bg-primary text-light" colspan="7">
-                        (2-20100) Trade Payable
-                      </td>
-                    </tr>
-                    <tr>
-                      <td class="bold">
-                        24/11/2019
-                      </td>
-                      <td class="text-left bold">
-                        Saldo Awal
-                      </td>
-                      <td colspan="4"></td>
-                      <td class="text-right bold">
-                        ( 500.000,00)
-                      </td>
-                    </tr>
-                    <tr class="report-subtotal">
-                      <td class="text-right regular-text" colspan="4">
-                        ((2-20100) Trade Payable) | Saldo Akhir
-                      </td>
-                      <td class="text-right bold">
-                        0,00
-                      </td>
-                      <td class="text-right bold">
-                        0,00
-                      </td>
-                      <td class="text-right bold">
-                        ( 500.000,00)
-                      </td>
-                    </tr>
-
-                    <tr>
-                      <td class="report-subheader-noindent bg-primary text-light" colspan="7">
-                        (2-20500) VAT Out
-                      </td>
-                    </tr>
-                    <tr>
-                      <td class="bold">
-                        24/11/2019
-                      </td>
-                      <td class="text-left bold">
-                        Saldo Awal
-                      </td>
-                      <td colspan="4"></td>
-                      <td class="text-right bold">
-                        1.000.000,00
-                      </td>
-                    </tr>
-                    <tr class="report-subtotal">
-                      <td class="text-right regular-text" colspan="4">
-                        ((2-20500) VAT Out) | Saldo Akhir
-                      </td>
-                      <td class="text-right bold">
-                        0,00
-                      </td>
-                      <td class="text-right bold">
-                        0,00
-                      </td>
-                      <td class="text-right bold">
-                        1.000.000,00
-                      </td>
-                    </tr>
-
-                    <tr>
-                      <td class="report-subheader-noindent bg-primary text-light" colspan="7">
-                        (5-50000) Cost of Sales
-                      </td>
-                    </tr>
-                    <tr>
-                      <td class="bold">
-                        24/11/2019
-                      </td>
-                      <td class="text-left bold">
-                        Saldo Awal
-                      </td>
-                      <td colspan="4"></td>
-                      <td class="text-right bold">
-                        ( 10.000.000,00)
-                      </td>
-                    </tr>
-                    <tr class="report-subtotal">
-                      <td class="text-right regular-text" colspan="4">
-                        ((5-50000) Cost of Sales) | Saldo Akhir
-                      </td>
-                      <td class="text-right bold">
-                        0,00
-                      </td>
-                      <td class="text-right bold">
-                        0,00
-                      </td>
-                      <td class="text-right bold">
-                        ( 10.000.000,00)
-                      </td>
-                    </tr>
-                    <tr class="bg-success text-light">
-                      <td class="text-right grand-total" colspan="4">
-                        Grand Total
-                      </td>
-                      <td class="text-right grand-total">
-                        0,00
-                      </td>
-                      <td class="text-right grand-total">
-                        0,00
-                      </td>
-                      <td class="text-right grand-total"></td>
-                    </tr>
-                  </tbody>
-                </table>
-              </div>
-              <div class="text-left show-total-transaction bold-roboto-text" style="">
-                Menampilkan total dari 4 baris transaksi
-              </div>
-            </div>
-          </div>
-          <div class="dt-responsive">
-            <table id="order-table" class="table table-striped table-bordered nowrap">
+          <div>
+            @php
+            function format_uang($angka){
+              $hasil =  number_format($angka,2, ',' , '.');
+              return $hasil;
+            }
+            @endphp
+            @foreach ($saldo_awal as $key)
+            <table class="table table-striped table-bordered nowrap">
               <thead>
                 <tr class="bg-secondary font-weight-bold">
-                  <th class="text-light">
-                    Nama Akun / Tanggal
-                  </th>
-                  <th class="text-light">
-                    Transaksi
-                  </th>
-                  <th class="text-light">
-                    Nomor
-                  </th>
-                  <th class="text-light">
-                    Keterangan
-                  </th>
-                  <th class="text-right text-light">
-                    Debit
-                  </th>
-                  <th class="text-right text-light">
-                    Kredit
-                  </th>
-                  <th class="text-right text-light">
-                    Saldo
-                  </th>
+                  <th class="col-8 text-light" colspan="4">Nama Akun: {{$key->account->nama}}</th>
+                  <th class="col-4 text-light" colspan="2">Nomor Akun: {{$key->account->nomor}}</th>
+                </tr>
+                <tr>
+                  <th class="col-2" rowspan="2">Tanggal</th>
+                  <th class="col-2" rowspan="2">Deskripsi</th>
+                  <th class="col-2" rowspan="2">Debet</th>
+                  <th class="col-2" rowspan="2">Kredit</th>
+                  <th class="col-4" colspan="2" style="text-align: center;">Balance</th>
+                </tr>
+                <tr>
+                  <th>Debet</th>
+                  <th>Kredit</th>
                 </tr>
               </thead>
               <tbody>
-                @php
-                function format_uang($angka){
-                  $hasil =  number_format($angka,2, ',' , '.');
-                  return $hasil;
-                }
-                @endphp
-                @foreach ($saldo_awal as $key)
-                  <tr>
-                    <tr>
-                      <td class="report-subheader-noindent bg-primary text-light" colspan="7">
-                        ({{ $key->account->nomor }}) {{ $key->account->nama }}
-                      </td>
-                    </tr>
-                    <tr>
-                      <td class="bold">
-                        {{$key->created_at}}
-                      </td>
-                      <td class="text-left bold">
-                        Saldo Awal
-                      </td>
-                      <td colspan="4"></td>
-                      <td class="text-right bold">
-                        Rp{{format_uang($key->debet)}}
-                      </td>
-                    </tr>
-                    <tr class="report-subtotal">
-                      <td class="text-right regular-text" colspan="4">
-                        ((1-10001) Cash) | Saldo Akhir
-                      </td>
-                      <td class="text-right bold">
-                        0,00
-                      </td>
-                      <td class="text-right bold">
-                        0,00
-                      </td>
-                      <td class="text-right bold">
-                        10.500.000,00
-                      </td>
-                    </tr>
-                  </tr>
-                @endforeach
+                <tr>
+                  <td>{{date('d F Y', strtotime($key->created_at ))}}</td>
+                  <td></td>
+                  <td>Rp{{format_uang($key->debet)}}</td>
+                  <td>Rp{{format_uang($key->kredit)}}</td>
+                  <td></td>
+                  <td></td>
+                </tr>
               </tbody>
             </table>
+          @endforeach
           </div>
         </div>
       </div>
