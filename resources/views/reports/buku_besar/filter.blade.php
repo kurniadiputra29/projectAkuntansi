db4e19405ea6d8f4b1f08ce490658def6a5201b0
adibest <ahmadjayadibya@gmail.com>
2020-01-14 21:02:06 +0700

    filter dan print buku besar

--- resources/views/reports/buku_besar/filter.blade.php
+++ resources/views/reports/buku_besar/filter.blade.php
@@ -40,10 +40,14 @@
                 <h3>Buku Besar</h3>
                 <span>use class <code>table-hover</code> inside table element</span>
               </div>
-              <div class="right-container">
-                <a type="button" class="btn btn-success mr-5" href="/laporan"><i class="ik ik-arrow-left"></i>Back</a>
-                <button type="button" class="btn btn-info mr-5" data-toggle="modal" data-target="#createModal"><i class="ik ik-filter"></i>Filter</button>
-                <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#pdfModal"><i class="ik ik-printer"></i>Print</button>
+              <div class="right-container d-flex flex-row">
+                <a type="button" class="btn btn-success mr-5" href="/buku_besar"><i class="ik ik-arrow-left"></i>Back</a>
+                <form class="" action="{{route('buku_besar.printF')}}" method="post">
+                  @csrf
+                  <input type="hidden" name="tanggal_mulai" value="{{$tanggal_mulai}}">
+                  <input type="hidden" name="tanggal_akhir" value="{{$tanggal_akhir}}">
+                  <button type="submit" class="btn btn-primary"><i class="ik ik-printer"></i>Print</button>
+                </form>
               </div>
             </div>
             <div class="card-body">
